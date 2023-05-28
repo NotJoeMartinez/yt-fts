966475569832ea1f41c5b75680c9e0b6ec1c0840
notjoemartinez <39905973+NotJoeMartinez@users.noreply.github.com>
2023-05-28 16:32:17 -0500

    Added readme to setup script

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     dependencies = f.read().splitlines()
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
 entry_points = {
     'console_scripts': [
         'yt-fts=yt_fts.yt_fts:cli',
@@ -11,8 +15,10 @@ entry_points = {
 
 setup(
     name='yt-fts', 
-    version='0.1.3',
-    description='yt-fts is a simple python script that uses yt-dlp to scrape all of a youtube channels subtitles and load them into an sqlite database that is searchable from the command line. It allows you to query a channel for specific key word or phrase and will generate time stamped youtube urls to the video containing the keyword.', 
+    version='0.1.4',
+    description='yt-fts is a simple python script that uses yt-dlp to scrape all of a youtube channels subtitles and load them into an sqlite database that is searchable from the command line. It allows you to query a channel for specific key word or phrase and will generate time stamped youtube urls to the video containing the keyword.',
+    long_description=long_description,
+    long_description_content_type='text/markdown', 
     author='NotJoeMartinez',
     url='https://github.com/NotJoeMartinez/yt-fts',  
     packages=find_packages(),
