8a097af979a8931580fec7f8c5b61b7d77a81dfc
notjoemartinez <39905973+NotJoeMartinez@users.noreply.github.com>
2023-06-03 20:33:17 -0500

    added coloring print format on all search methods

--- yt_fts/search_utils.py
+++ yt_fts/search_utils.py
@@ -1,5 +1,40 @@
 from yt_fts.db_utils import * 
 from yt_fts.utils import *
+from rich.console import Console
+
+
+def print_search_results(res):
+
+    """
+    format of res:
+    [
+        {
+            "channel_name": "channel_name",
+            "video_title": "video_title",
+            "subs": "subs",
+            "time_stamp": "time_stamp",
+            "video_id": "video_id",
+            "link": "link"
+        }
+    ]
+    """
+    # sort by channel name
+    res = sorted(res, key=lambda x: x["channel_name"])
+
+    console = Console()
+
+    console.print("")
+    for quote in res: 
+
+        console.print(f"[magenta]\"[bold][link={quote['link']}]{quote['subs']}[/link][/bold]\"[/magenta]", style="magenta")
+        console.print(f"    Channel: {quote['channel_name']}",style="none")
+        console.print(f"    Title: {quote['video_title']}",style="none")
+        print(f"    Time Stamp: {quote['time_stamp']}")
+        console.print(f"    Video ID: {quote['video_id']}")
+        console.print(f"    Link: {quote['link']}")
+        console.print("")
+            
+
 
 def get_text(channel_id, text):
     """
@@ -14,42 +49,50 @@ def get_text(channel_id, text):
         show_message("no_matches_found")
         exit()
 
+    fts_res = []
     for quote in res:
+
+        quote_match = {}
+
         video_id = quote["video_id"]
-        subs = quote["text"]
         time_stamp = quote["timestamp"]
-        video_title = get_title_from_db(video_id)
-        channel_name = get_channel_name_from_video_id(video_id)
-        time = time_to_secs(time_stamp) 
+        time = time_to_secs(time_stamp)
+        link = f"https://youtu.be/{video_id}?t={time}"
 
-        print("")
-        print(f"{channel_name}: \"{video_title}\"")
-        print(f"") 
-        print(f"    Quote: \"{subs.strip()}\"")
-        print(f"    Time Stamp: {time_stamp}")
-        print(f"    Video ID: {video_id}")
-        print(f"    Link: https://youtu.be/{video_id}?t={time}\n")
+        quote_match["channel_name"] = get_channel_name_from_video_id(video_id)
+        quote_match["video_title"] = get_title_from_db(video_id)
+        quote_match["subs"] = quote["text"].strip()
+        quote_match["time_stamp"] = time_stamp
+        quote_match["video_id"] = video_id
+        quote_match["link"] = link 
 
+        fts_res.append(quote_match)
+
+    print_search_results(fts_res)
 
 def get_text_by_video_id(video_id, text):
     res = search_video(video_id, text)
     if len(res) == 0:
         show_message("no_matches_found")
         exit()
-    
+
+    fts_res = []
     for quote in res:
-        subs = quote["text"]
+
+        quote_match = {}
+
+        video_id = quote["video_id"]
         time_stamp = quote["timestamp"]
-        video_title = get_title_from_db(video_id)
-        channel_name = get_channel_name_from_video_id(video_id)
-        time = time_to_secs(time_stamp) 
+        time = time_to_secs(time_stamp)
+        link = f"https://youtu.be/{video_id}?t={time}"
 
-        print("")
-        print(f"{channel_name}: \"{video_title}\"")
-        print(f"") 
-        print(f"    Quote: \"{subs.strip()}\"")
-        print(f"    Time Stamp: {time_stamp}")
-        print(f"    Video ID: {video_id}")
-        print(f"    Link: https://youtu.be/{video_id}?t={time}\n")
+        quote_match["channel_name"] = get_channel_name_from_video_id(video_id)
+        quote_match["video_title"] = get_title_from_db(video_id)
+        quote_match["subs"] = quote["text"].strip()
+        quote_match["time_stamp"] = time_stamp
+        quote_match["video_id"] = video_id
+        quote_match["link"] = link 
 
+        fts_res.append(quote_match)
 
+    print_search_results(fts_res)
--- yt_fts/semantic_serch/search_embeddings.py
+++ yt_fts/semantic_serch/search_embeddings.py
@@ -1,12 +1,16 @@
 import sqlite3 
 import pickle
-import numpy as np
 import heapq
+
+import numpy as np
+
 from sklearn.metrics.pairwise import cosine_similarity
+from rich.console import Console
+
 from yt_fts.utils import time_to_secs
 from yt_fts.search_utils import get_channel_name_from_video_id
 from yt_fts.db_utils import get_title_from_db
-
+from yt_fts.search_utils import print_search_results
 
 def search_using_embedding(search_embedding, top_n, channel_id=None):
     con = sqlite3.connect('subtitles.db')
@@ -31,9 +35,11 @@ def search_using_embedding(search_embedding, top_n, channel_id=None):
     con.close()
 
 
-    for sim, row in sorted(heap, reverse=True):
+    semantic_res = []
 
-        print("")
+    for sim, row in sorted(heap, reverse=True):
+        quote_match = {}
+
         quote = row[3]
         timestamp = row[2]
         time = time_to_secs(timestamp)
@@ -41,11 +47,16 @@ def search_using_embedding(search_embedding, top_n, channel_id=None):
         link = f"https://youtu.be/{video_id}?t={time}"
         channel_name = get_channel_name_from_video_id(video_id)
         video_title = get_title_from_db(video_id)
+        
+        quote_match["channel_name"] = channel_name
+        quote_match["video_title"] = video_title
+        quote_match["subs"] = quote
+        quote_match["time_stamp"] = timestamp
+        quote_match["video_id"] = video_id
+        quote_match["link"] = link
 
-        print("")
-        print(f"{channel_name}: \"{video_title}\"")
-        print(f"") 
-        print(f"    Quote: \"{quote}\"")
-        print(f"    Time Stamp: {timestamp}")
-        print(f"    Video ID: {video_id}")
-        print(f"    Link: {link}")
+        semantic_res.append(quote_match)
+
+    print_search_results(semantic_res)
+
+
