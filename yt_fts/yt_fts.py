078650ebccda38651d323adf4ee4c57c7e1d2e6f
notjoemartinez <39905973+NotJoeMartinez@users.noreply.github.com>
2023-08-01 03:03:59 -0500

    eslint stuff

--- yt_fts/yt_fts.py
+++ yt_fts/yt_fts.py
@@ -68,29 +68,25 @@ def download(channel_url, language, number_of_jobs):
 
     """__Schrodinger's Channel ID__
 
-    Alright, Now that we know it exists on the internet, 
-    let's find out if we had it all along to begin with! 
-    What's that you're saying? We should have checked this 
-    before making the request? 
-
-    Well, that's just because you're not thinking like a
-    10x developer.
+    Alright, Now that we know it exists on the internet, let's find out 
+    if we had it all along to begin with! What's that you're saying? 
+    We should have checked this before making the request? Well, that's 
+    just because you're not thinking like a 10x developer.
     
     You see, I program in a state of quantum superposition. 
     
-    While you might think it's more efficient to check if 
-    the data exists in the local database before wasting 
-    bandwidth making a request to the internet.
-
-    A Superpositionist, such as myself, knows that the data 
+    While you might think it's more efficient to check if the data exists 
+    in the local database before wasting bandwidth making a request to 
+    the internet. A Superpositionist, such as myself, knows that the data 
     actually exists in both states at the same time! 
      
-    However, it is only when we observe the data that it collapses 
-    into a single state. And because our database was designed 
-    in such brilliant way that we must first observe remote data 
-    to actually know if it exists locally, one might say that 
+    However, it is only when we observe the data that it collapses into a 
+    single state. And because our database was designed in such brilliant 
+    way that we must first observe remote data to actually know if it exists,
 
-    we are in a state of quantum entanglement with the data.
+    one might say that. 
+
+    We are in a state of quantum entanglement with the data.
 
     """
     channel_exists = check_if_channel_exists(channel_id)
